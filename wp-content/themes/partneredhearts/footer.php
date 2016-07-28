2e1f4e834fc891cc84047429c40b044f27e02566
Daniel Valenzuela <daniel.valenzuela@4thsource.com>
2016-07-28 18:25:22 -0500

    Edits 2016-07-28

--- wp-content/themes/partneredhearts/footer.php
+++ wp-content/themes/partneredhearts/footer.php
@@ -3,9 +3,11 @@
 		<!-- footer -->
 		<footer class="footer" style="min-height:<?PHP the_field('footer_height','option'); ?>px; background-color:lightgray;">	
             <div class="container-fluid" style="min-height:<?PHP the_field('footer_height','option'); ?>px; background-color:#6D6E70;">   
-                <div class="row">
-                    <div class="col-sm-7 footer-top-padding">
+                <div class="row no-gutter">
+                    <div class="col-sm-2 footer-top-padding">
                         <div class="sponser-logo footer-left-padding">OUR SPONSERS:</div>
+					</div>
+                    <div class="col-sm-5 footer-top-padding">
 						<?PHP while ( have_rows('sponsers', 'option') ) : the_row(); $image = get_sub_field('sponser_image'); $link = get_sub_field('sponser_link'); $name = get_sub_field('sponser_name'); ?>
 						<div class="sponser-logo">
 							<?PHP if (get_sub_field('sponser_link')!=''): ?>
@@ -20,7 +22,7 @@
                         <div style="display:inline-block;" >
 							<?PHP echo do_shortcode(get_field('footer_subscribe_contact_form','option')); ?>
                         </div>
-                        <div style="float:right;" class="footer-align-right footer-right-padding">						
+                        <div class="footer-align-right footer-right-padding">						
 							<?PHP if (get_field('facebook_link','option')!=''): ?>
                             <a href="<?PHP the_field('facebook_link','option') ?>" class="footer-social-icon" target="_blank"><img alt="" src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" /></a>						
 							<?PHP endif; if (get_field('twitter_link','option')!=''): ?>
